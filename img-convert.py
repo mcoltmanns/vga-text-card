cc3c02d0915af61c3b2e353e70a05d50b65ae03c
Max Oltmanns <max.oltmanns@uni-konstanz.de>
2023-10-04 12:16:16 +0200

    functional textless mode at 160x120

--- img-convert.py
+++ img-convert.py
@@ -10,13 +10,17 @@ for infile in sys.argv[1:]:
     
     width, height = im.size
 
-    for x in range(width):
-        for y in range(height):
-            px = im.getpixel((y,x)) # not sure why these have to be backwards
+    for y in range(120):
+        for x in range(160):
+            px = im.getpixel((x,y)) # not sure why these have to be backwards
             # 3 bits for r, 3 for g, 2 for b
             r = (px[0] >> 5) << 5
             g = (px[1] >> 5) << 2
             b = (px[2] >> 6)
             out.write((r+g+b).to_bytes(1, 'little'))
+        # write padding bytes at end of each row
+        for a in range(96):
+            p = 0
+            out.write(p.to_bytes(1))
 
     out.close()
