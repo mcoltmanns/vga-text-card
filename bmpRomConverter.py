56d159731d281493ad7bb8de5ed78f15aaf5554a
Max Oltmanns <max.oltmanns@uni-konstanz.de>
2023-10-10 13:03:45 +0200

    got text mode working

---
+++ bmpRomConverter.py
@@ -0,0 +1,28 @@
+import os, sys, math
+from PIL import Image
+
+# expects a 128x128 bitmap png of an 8x8 font
+# dwarf fortress tilesets are a great place to find these!
+
+for infile in sys.argv[1:]:
+    f, e = os.path.splitext(infile)
+    outfile = f + ".rom"
+
+    bmp = Image.open(f + ".png")
+    rom = open(outfile, "wb")
+
+    width, height = bmp.size
+
+    for y in range(16):
+        for x in range(16):
+            for charY in range(8):
+                for charX in range(8):
+                    r, g, b, a = bmp.getpixel((x * 8 + charX, y * 8 + charY))
+                    addr = (x << 12) + (y << 8) + (charY << 4) + (charX)
+                    pxval = 0
+                    if r == g == b:
+                        pxval = ((r >> 5) << 5) + ((g >> 5) << 2) + (b >> 6)
+                    rom.write(pxval.to_bytes(1, 'little'))
+                # padding bytes
+                p = 0
+                rom.write(p.to_bytes(8))
